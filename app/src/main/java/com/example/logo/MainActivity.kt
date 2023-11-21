7110a3770664d1faa13568e16de7145cf177fd69
Wulan Ayu Febriani <wulanayufebriani@gmail.com>
2023-11-21 23:03:08 +0700

    login

---
+++ app/src/main/java/com/example/logo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.logo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    //timer
+    private val timeout: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+     Handler().postDelayed({
+         startActivity(Intent(this, login::class.java))
+         finish()
+     }, timeout)
+
+
+
+    }
+}
