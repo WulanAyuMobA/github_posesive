7045ba0c3c566ce5f33a2c70ee6019a5f0316f1a
FRANCISCUS XAVERIUS ANDIKA HARDITO <202165016@student.unipa.ac.id>
2023-11-23 09:47:52 +0900

    Version 1.1

---
+++ app/src/main/java/com/example/logo/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.example.logo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+
+class HomeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+
+    }
+}
--- app/src/main/java/com/example/logo/login.kt
+++ app/src/main/java/com/example/logo/login.kt
@@ -1,11 +1,31 @@
 package com.example.logo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class login : AppCompatActivity() {
+class login : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+         btn = findViewById<Button>(R.id.btn_logindisini)
+
+        btn.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btn_logindisini ->{
+                val intentb =  Intent(this@login,HomeActivity::class.java)
+                startActivity(intentb)
+            }
+        }
     }
 }
