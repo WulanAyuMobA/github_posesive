c593c79ad059893b4d1f5d78a4dddd0f51072dad
FRANCISCUS XAVERIUS ANDIKA HARDITO <202165016@student.unipa.ac.id>
2023-11-23 10:11:35 +0900

    Version 1.1

--- app/src/main/java/com/example/logo/HomeActivity.kt
+++ app/src/main/java/com/example/logo/HomeActivity.kt
@@ -1,17 +1,40 @@
 package com.example.logo
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
+import android.view.View
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    private lateinit var profile: CircleImageView
+    private lateinit var logistik: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        profile = findViewById<CircleImageView>(R.id.Fprofil)
+        logistik = findViewById<TextView>(R.id.tv_logistik2)
+        profile.setOnClickListener(this)
+        logistik.setOnClickListener(this)
 
+    }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.Fprofil -> {
+                val intentb = Intent(this@HomeActivity, ProfileActivity::class.java)
+                startActivity(intentb)
+            }
+
+            R.id.tv_logistik2 -> {
+                val intentb = Intent(this@HomeActivity, LogistikBarangActivity::class.java)
+                startActivity(intentb)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/logo/ProfileActivity.kt
+++ app/src/main/java/com/example/logo/ProfileActivity.kt
@@ -1,11 +1,29 @@
 package com.example.logo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btneditp : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        btneditp = findViewById<Button>(R.id.EditProfileButton)
+
+        btneditp.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.EditProfileButton ->{
+                val intentb =  Intent(this@ProfileActivity,EditProfileActivity::class.java)
+                startActivity(intentb)
+            }
+        }
     }
 }
